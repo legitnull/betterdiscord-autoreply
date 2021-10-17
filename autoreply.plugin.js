070ef15eca5c7f0627fcc5e9c9ffc06a92d79066
legitnull <dev@uwucoin.net>
2021-10-17 10:31:20 -0600

    added descriptors

--- autoreply.plugin.js
+++ autoreply.plugin.js
@@ -1,11 +1,25 @@
 /**
- * @name Auto-Reply
+ * @name AutoReply
  * @version 0.0.1
  * @description This plugin allows you to auto reply to Direct Messages
  * @author legitnull
- *  
+ * @source https://github.com/legitnull/betterdiscord-autoreply/blob/master/autoreply.plugin.js
  */
-
+const config = {
+    info: {
+        name: 'AutoReply',
+        authors: [
+            {
+                name: 'legitnull',
+                github_username: 'legitnull'
+            }
+        ],
+        version: '0.0.1',
+        description: 'This plugin allows you to auto reply to Direct Messages',
+        github: 'https://github.com/legitnull/betterdiscord-autoreply/',
+        github_raw: 'https://raw.githubusercontent.com/legitnull/betterdiscord-autoreply/master/autoreply.plugin.js'
+    }
+};
 
 
  module.exports = class Example{
