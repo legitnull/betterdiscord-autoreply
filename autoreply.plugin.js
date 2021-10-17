6f8e5973260d1a1f74e44bfed60e1f7e257fdd81
legitnull <dev@uwucoin.net>
2021-10-17 09:16:55 -0600

    adding plugin file

---
+++ autoreply.plugin.js
@@ -0,0 +1,21 @@
+/**
+ * @name Auto-Reply
+ * @version 0.0.1
+ * @description This plugin allows you to auto reply to Direct Messages
+ * @author legitnull
+ *  
+ */
+
+
+
+ module.exports = class Example{
+     
+    load() { }
+    start() {
+//begining
+
+}
+stop(){
+
+}
+}
